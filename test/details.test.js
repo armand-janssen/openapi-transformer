9481e1a38980cee4eab2e15b57795a795e06e8fc
Armand Janssen <armandjanssen1978@gmail.com>
2019-05-10 13:04:33 +0200

    Added test structure

--- test/details.test.js
+++ test/details.test.js
@@ -1,10 +1,12 @@
 const { assert } = require('chai');
 const Detail = require('../src/detail');
 
-describe('Test constructor', () =>{
-  const expectedName = 'some-name';
-  const expectedValue = 'some-value';
-  const detail = new Detail(expectedName, expectedValue);
-  assert.equal(detail.name, expectedName);
-  assert.equal(detail.value, expectedValue);
+describe('Test constructor', () => {
+  it('Test constructor', () => {
+    const expectedName = 'some-name';
+    const expectedValue = 'some-value';
+    const detail = new Detail(expectedName, expectedValue);
+    assert.equal(detail.name, expectedName);
+    assert.equal(detail.value, expectedValue);
+  });
 });
--- test/relationship.test.js
+++ test/relationship.test.js
@@ -1,14 +1,16 @@
 const { assert } = require('chai');
 const Relationship = require('../src/relationship');
 
-describe('Test constructor', () =>{
-  const expectedFrom = 'some-from';
-  const expectedTo = 'some-to';
-  const expectedDescription = 'some-description';
-  const expectedType = 'some-type';
-  const detail = new Relationship(expectedFrom, expectedTo,expectedDescription,expectedType);
-  assert.equal(detail.from, expectedFrom);
-  assert.equal(detail.to, expectedTo);
-  assert.equal(detail.description, expectedDescription);
-  assert.equal(detail.type, expectedType);
+describe('Test constructor', () => {
+  it('Test constructor', () => {
+    const expectedFrom = 'some-from';
+    const expectedTo = 'some-to';
+    const expectedDescription = 'some-description';
+    const expectedType = 'some-type';
+    const detail = new Relationship(expectedFrom, expectedTo, expectedDescription, expectedType);
+    assert.equal(detail.from, expectedFrom);
+    assert.equal(detail.to, expectedTo);
+    assert.equal(detail.description, expectedDescription);
+    assert.equal(detail.type, expectedType);
+  });
 });
