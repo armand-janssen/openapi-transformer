20d9bd14492f359e491bd10e7af18e68a9d833de
Armand Janssen <armand.janssen@hyfen.nl>
2022-05-10 08:34:36 +0200

    fixed missing semi colon

--- src/jsonSchemaTransformer.js
+++ src/jsonSchemaTransformer.js
@@ -38,7 +38,7 @@ function generateProperty(property, listOfUsedSchemas) {
   } else if (property.type === 'date-time') {
     js.type = 'string';
     js.format = 'date-time';
-    js.pattern = '^([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T[0-5][0-9]:[0-5][0-9]:[0-5][0-9]Z$'
+    js.pattern = '^([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T[0-5][0-9]:[0-5][0-9]:[0-5][0-9]Z$';
     processDetails = false;
   } else if (property.type.indexOf('array[] of ') > -1) {
     js.type = 'array';
