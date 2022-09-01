db2b05e9bd9f196ba2ddc19eeede90cf150a354f
ABDELHAMID Ahmed OBS/CMI <ahmed_fcis@hotmail.com>
2022-09-01 18:49:11 +0200

    add support to extract from root type with configurable depth

--- src/cli.js
+++ src/cli.js
@@ -9,12 +9,15 @@ const markdownTransformer = require('./markdownTransformer');
 const jsonSchemaTransformer = require('./jsonSchemaTransformer');
 const csvTransformer = require('./csvTransformer');
 const openApiGenerator = require('./index.js');
+const filter = require('./filter');
 
 program
   .version(version)
   .usage('[options] <inputFileOrUrl>')
   .description('At least 1 output type must be selected: plantuml or markdown!')
   .option('-d, --details', 'Show extra attribute details')
+  .option('-r --root <root type>', 'Specify root class to start extracting from')
+  .option('--depth <depth>', 'Specify extracting depth')
   .option('-p, --plantuml <plantuml file>', 'Transform to plantuml')
   .option('-m, --markdown <markdown file>', 'Transform to markdown')
   .option('-c, --csv <csv file>', 'Transform to csv')
@@ -29,34 +32,41 @@ if (!program.args.length || (program.plantuml == null && program.markdown == nul
 
   if (verbose) console.log('Reading openAPI...');
   (async () => {
-    const allParsedSchemas = await openApiGenerator.loadYamlFile(program.args[0], verbose);
+    const _allParsedSchemas = await openApiGenerator.loadYamlFile(program.args[0], verbose);
+
+    let parsedSchemas = _allParsedSchemas;
+    
+    if(program.root != null){
+      let depth = program.depth || 100;
+      parsedSchemas = filter.filterByMainSchema(_allParsedSchemas,program.root,depth);
+    }
 
     if (program.plantuml !== undefined) {
       if (verbose) console.log('Writing plantuml...');
       let uml = '';
       if (program.details === undefined) {
-        uml = plantUmlTransformer.generate(allParsedSchemas, false);
+        uml = plantUmlTransformer.generate(parsedSchemas, false);
       } else {
-        uml = plantUmlTransformer.generate(allParsedSchemas, true);
+        uml = plantUmlTransformer.generate(parsedSchemas, true);
       }
       fs.writeFileSync(program.plantuml, uml, 'utf8');
     }
 
     if (program.markdown !== undefined) {
       if (verbose) console.log('Writing markdown...');
-      const md = markdownTransformer.generate(allParsedSchemas);
+      const md = markdownTransformer.generate(parsedSchemas);
       fs.writeFileSync(program.markdown, md, 'utf8');
     }
 
     if (program.jsonschema !== undefined) {
       if (verbose) console.log('Writing JSON Schema...');
-      const js = jsonSchemaTransformer.generate(allParsedSchemas);
+      const js = jsonSchemaTransformer.generate(parsedSchemas);
       fs.writeFileSync(program.jsonschema, js, 'utf8');
     }
 
     if (program.csv !== undefined) {
       if (verbose) console.log('Writing CSV Schema...');
-      const csv = csvTransformer.generate(allParsedSchemas);
+      const csv = csvTransformer.generate(parsedSchemas);
       fs.writeFileSync(program.csv, csv, 'utf8');
     }
 
